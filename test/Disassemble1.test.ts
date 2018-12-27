fb7d1f82936d9b4b1e77321026ed74682a301997
Jose <j.mingorance@crazy-factory.com>
2018-12-27 17:34:58 +0100

    Adds an unit test

--- test/Disassemble1.test.ts
+++ test/Disassemble1.test.ts
@@ -1,11 +1,5 @@
-import { WasmDisassembler } from '../lib/WasmDisassembler';
-import { WasmMemoryProvider } from '../lib/binaryProvider/WasmMemoryProvider';
-import * as fs from 'fs'
+
 import * as mocha from 'mocha';
-import * as chai from 'chai';
-
-const expect = chai.expect;
-
 
 mocha.describe('Tests Disassembler', () => {
 
---
+++ test/WasmMemoryProvider.test.ts
@@ -0,0 +1,18 @@
+import { WasmMemoryProvider } from '../lib/binaryProvider/WasmMemoryProvider';
+
+import * as mocha from 'mocha';
+import * as chai from 'chai';
+
+const expect = chai.expect;
+mocha.describe('Tests WasmMemoryProvider', () => {
+
+    it('Can create an WasmMemoryProvider from an Uint8Array', () => {
+        const byteArray = new Uint8Array(10)
+        let wasmBinaryProvider = new WasmMemoryProvider(byteArray);
+        expect(wasmBinaryProvider).not.undefined
+        expect(wasmBinaryProvider).not.null
+        expect(wasmBinaryProvider.Length).not.undefined
+        expect(wasmBinaryProvider.Length).to.be.equal(byteArray.length)
+    });
+
+});
